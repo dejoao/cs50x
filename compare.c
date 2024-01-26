59db63ac4163006d6beb63798bdd49cc10b5ab09
Joao <joaopedro149893@gmail.com>
2024-01-25 22:53:13 -0300

    add: else if

--- compare.c
+++ compare.c
@@ -5,10 +5,17 @@ int main(void) {
     int x = get_int("Qual x?");
     int y = get_int("Qual y?");
 
-    if (x < y)
-    {
+    if (x < y) {
         printf("x e menor que y \n");
     }
+
+    else if (x > y) {
+        printf("x e maior que y \n");
+        }
+
+    else if (x == y) {
+            printf("x e igual a y \n");
+        }
     
     return 0;
 }
