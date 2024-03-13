d35ec17444bb37522ac9c080a8c966ff4933e776
Joao <joaopedro149893@gmail.com>
2024-03-13 01:15:38 -0300

    add: Problema Recover

---
+++ recover/recover.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[])
+{
+    //Aceite uma unica linha de comando
+    char argumento = argv[1];
+    char VerificacaoCard = "card.raw";
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+    //Abra o cartao de memoria
+    FILE *card = fopen("card.raw", "r");
+    if (card == NULL)
+    {
+        printf("Error\n");
+        return 1;
+    }
+    //Buffer para um bloco de dados
+    int bloco = 512;
+    uint8_t buffer[bloco];
+
+    //Crie jpegs a partir dos dados
+    char c;
+    fread(&c, sizeof(char[3]), 4, &card);
+    
+}
