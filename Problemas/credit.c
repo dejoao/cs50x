929a36af90993dfc91d6f6fb3a05f9236a32aa89
Joao <joaopedro149893@gmail.com>
2024-09-28 00:10:40 -0300

    add: comecando validador

--- Problemas/credit.c
+++ Problemas/credit.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <cs50.h>
 
-long long numeroCartao;
+string numeroCartao;
 
 int main (void){
     //Solicita ao usuario o numero de cartao
-    numeroCartao = get_long_long("Numero: ");
+    numeroCartao = get_string("Numero: ");
     
 }
 
-int validador (int cartao){
+int validador (string cartao[]){
+    /*inverte o numero do cartao. Preciso passar por cada caracter da string
+    comecando do final, e adicionar os caraceteres em uma nova  string */
     
+    int caracterCartao = 0;
+    for (int i = 0; i < cartao[caracterCartao]; i++)
+    {
+        caracterCartao++;
+    }
+
+    //Seledcionar os digitos para multiplicar por dois cada digito e, depois somar
+    
+    //Somar o resultado com os numeros q nao foram multiplicados por dois
+
+
 }
