3f6d07742dacd3b6bdd8f5715c9ec7546421dbd0
Joao <joaopedroaraujodeoliveira14@gmail.com>
2024-10-02 21:41:25 -0300

    add: funcionando conversao de long long para string

--- Problemas/credit2.c
+++ Problemas/credit2.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 int validador(char cartaoConvertido[]);
+int caracteres (long long cartao);
 
 long long cartao;
 
@@ -16,29 +17,37 @@ int main(void){
         cartao = get_long_long("Numero: ");
     } while (cartao < 1000000000000 || cartao < -0 || cartao > 10000000000000000);
     //Preciso converter o cartao para uma string, so assim posso manipular
-    int *indice;
-    indice = malloc(sizeof(long long));
-    char cartaoConvertido[*indice];
+    char cartaoConvertido[caracteres(cartao)];
     sprintf(cartaoConvertido, "%lli", cartao);
     
     validador(cartaoConvertido);
-    //free(indice);
+}
+
+int caracteres (long long cartao){
+    int count = 0;
+    while (cartao != 0){
+        cartao = cartao/10;
+        count++;
+    }
+    return count;
 }
 
 int validador(char cartaoConvertido[]){
     // Inverter o numero do cartao
-    char cartaoInvertido[strlen(cartaoConvertido)];
+    int qCaracteresCartao = strlen(cartaoConvertido) + 1;
+    printf("%i\n", qCaracteresCartao);
+    char cartaoInvertido[qCaracteresCartao];
     int indiceCI = 0;
-    int indiceCC = strlen(cartaoConvertido);
+    int indiceCC = qCaracteresCartao - 2;
 
-    for(int i = 0; i < strlen(cartaoConvertido) ; i++){
+    for(int i = 0; i < qCaracteresCartao ; i++){
 
         cartaoInvertido[indiceCI] = cartaoConvertido[indiceCC];
         indiceCI++;
         indiceCC--;
 
     }
-    printf("%s", cartaoInvertido);
+    //printf("%s\n", cartaoInvertido);
+
+    //
 }
-/* Tenho que inverter a string; Na parte de converter o long long em string
-"deu certo", preciso ve onde vou colocar o free para liberar a memoria.   */
