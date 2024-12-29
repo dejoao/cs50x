890dec04420e5deb72962648b4b69f48fd2fbe3e
Joao <joaopedro149893@gmail.com>
2024-12-29 00:17:24 -0300

    add criacao de jpegs

--- Week 4 - Memory/recover/recover1.c
+++ Week 4 - Memory/recover/recover1.c
@@ -27,24 +27,49 @@ int main(int argc, char *argv[]){
     //Bufer para armazenar os dados lido
     uint8_t dados_lidos[bloco];
 
-    int indice = 0;
+    //int indice = 0;
 
     //Loop para ler os dados do cartao de memoria
     while(fread(dados_lidos, 1, bloco, card) == bloco){
-        
+        int indice = 0;
+
         if(dados_lidos[indice] == 0xff){
             if(dados_lidos[indice + 1] == 0xd8){
                 if(dados_lidos[indice + 2] == 0xff){
-                    if(dados_lidos[indice + 3] == 0xe0 || 0xe1 || 0xe2 || 0xe3 || 0xe4 || 0xe5 || 0xe6 || 0xe7 || 0xe8 || 0xe9 || 0xea || 0xeb || 0xec || 0xed || 0xee || 0xef){
+                    if(dados_lidos[indice + 3] == 0xe0 || dados_lidos[indice + 3] ==  0xe1 || dados_lidos[indice + 3] ==  0xe2 || dados_lidos[indice + 3] ==  0xe3 || dados_lidos[indice + 3] ==  0xe4 || dados_lidos[indice + 3] ==  0xe5 || dados_lidos[indice + 3] ==  0xe6 || dados_lidos[indice + 3] ==  0xe7 || dados_lidos[indice + 3] ==  0xe8 || dados_lidos[indice + 3] ==  0xe9 || dados_lidos[indice + 3] ==  0xea || dados_lidos[indice + 3] ==  0xeb || dados_lidos[indice + 3] ==  0xec || dados_lidos[indice + 3] ==  0xed || dados_lidos[indice + 3] ==  0xee || dados_lidos[indice + 3] ==  0xef){
                         FILE *imagem = fopen("001.jpg", "w");
                         //Usar funcao fwrite para pegar o bytes do cartao e escrever no arquivo jpeg criado
+                        fwrite(dados_lidos, 1, bloco, imagem);
                     }
                 }
             }
         }
-        indice++;  
+        indice++;
+         
     }
+    fclose(imagem);
+
+    if(fread(dados_lidos, 1, bloco, card) < bloco){
+        
+        while(fread(dados_lidos, 1, bloco, card) == bloco){
+            int indice = 0;
+
+            if(dados_lidos[indice] == 0xff){
+                if(dados_lidos[indice + 1] == 0xd8){
+                    if(dados_lidos[indice + 2] == 0xff){
+                        if(dados_lidos[indice + 3] == 0xe0 || dados_lidos[indice + 3] ==  0xe1 || dados_lidos[indice + 3] ==  0xe2 || dados_lidos[indice + 3] ==  0xe3 || dados_lidos[indice + 3] ==  0xe4 || dados_lidos[indice + 3] ==  0xe5 || dados_lidos[indice + 3] ==  0xe6 || dados_lidos[indice + 3] ==  0xe7 || dados_lidos[indice + 3] ==  0xe8 || dados_lidos[indice + 3] ==  0xe9 || dados_lidos[indice + 3] ==  0xea || dados_lidos[indice + 3] ==  0xeb || dados_lidos[indice + 3] ==  0xec || dados_lidos[indice + 3] ==  0xed || dados_lidos[indice + 3] ==  0xee || dados_lidos[indice + 3] ==  0xef){
+                            FILE *imagem = fopen("002.jpg", "w");
+                            //Usar funcao fwrite para pegar o bytes do cartao e escrever no arquivo jpeg criado
+                            fwrite(dados_lidos, 1, bloco, imagem);
+                        }
+                    }
+                }
+            }
+            indice++;
+         
+        }
+    }  
 
 }
-//tres primeiro bytes: 0xff 0xd8 0xff. opcoes do quarto byte 0xe0 0xe1 0xe2 0xe3 0xe4 0xe5 0xe6 0xe7 0xe8 0xe9 0xea 0xeb 0xec 0xed 0xee, ou 0xef.
-//Fix linha 38 da para melhorar e nome dos arquivos 
+//Preciso nomear os aquivos jpeg criados dinamicamente, é o proximo passo para avançar
+//Fix linha 38 da para melhorar, e nome dos arquivos 
