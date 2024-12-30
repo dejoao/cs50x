7f1dbcc414acdf8e51b6027c688d89f3b2e85fbd
Joao <joaopedro149893@gmail.com>
2024-12-30 00:14:37 -0300

    fix condicao linha 37

--- Week 4 - Memory/recover/recover.c
+++ Week 4 - Memory/recover/recover.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]){
     //Loop para ler os dados do cartao de memoria
     while(fread(dados_lidos, 1, bloco, card) == bloco){
         
-        if(dados_lidos[0] == 0xff && dados_lidos[1] == 0xd8 && dados_lidos[2] == 0xff && dados_lidos[3] == 0xe0 || dados_lidos[3] ==  0xe1 || dados_lidos[3] ==  0xe2 || dados_lidos[3] ==  0xe3 || dados_lidos[3] ==  0xe4 || dados_lidos[3] ==  0xe5 || dados_lidos[3] ==  0xe6 || dados_lidos[3] ==  0xe7 || dados_lidos[3] ==  0xe8 || dados_lidos[3] ==  0xe9 || dados_lidos[3] ==  0xea || dados_lidos[3] ==  0xeb || dados_lidos[3] ==  0xec || dados_lidos[3] ==  0xed || dados_lidos[3] ==  0xee || dados_lidos[3] ==  0xef){
+        if((dados_lidos[0] == 0xff && dados_lidos[1] == 0xd8 && dados_lidos[2] == 0xff && (dados_lidos[3] >= 0xe0 && dados_lidos[3] <= 0xef))){
             //fecha jpeg anterior, se houver
             if(imagem != NULL){
                 fclose(imagem);
